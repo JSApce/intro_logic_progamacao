c73b24c8a9935131f453f76a0837d7b14e4b1b96
jsapce <jamerson.developer@gmail.com>
2020-10-12 10:56:38 -0300

    Elaborating an algorithm in Eclipse

--- src/CalculaValor.java
+++ src/CalculaValor.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class CalculaValor {
 
 	public static void main(String[] args) {
-		System.out.println("olá");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o valor do produto: ");
+		Double valoProduto = scanner.nextDouble();
+		
+		System.out.print("Digite o valor informado pelo cliente: ");
+		Double valoInformadoPelocliente = scanner.nextDouble();
+		
+		
+		System.out.println("Troco: " + (valoInformadoPelocliente - valoProduto));
+		
+		scanner.close();
 	}
 }
