1da579e813bbd978f7999bd38b24570ed2c99aaa
jsapce <jamerson.developer@gmail.com>
2020-10-12 17:30:34 -0300

    Calculando o quadrado de um número

--- src/CalculaValor.java
+++ src/CalculaValor.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class CalculaValor {
 
 	public static void main(String[] args) {
+		/*
+		 * Leia um número no console e depois faça o cálculo do quadrado desse número e
+		 * exiba o resultado no console. Lembrando que o quadrado de um número é
+		 * calculado multiplicando-se o número por ele mesmo. Por exemplo, para
+		 * encontrar o quadrado de 2 é preciso multiplicar o 2 por 2, tendo como
+		 * resultado o número 4.
+		 */
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Calculo do IMC");
-		System.out.print("Digite a altura: ");
-		Double valorAltura = scanner.nextDouble();
-		
-		System.out.print("Digite o peso: ");
-		Double valorPeso = scanner.nextDouble();
-		
-		Double imc = valorPeso / (valorAltura * valorAltura);
-		System.out.println("O IMC é: " + imc);
-		
+		System.out.println("Calculando o quadrado de um número");
+		System.out.print("Digite um número inteiro: ");
+
+		Integer result = scanner.nextInt();
+		System.out.println("O quadrado de " + result + " é: "  + (result * result));
+
 		scanner.close();
 	}
 }
