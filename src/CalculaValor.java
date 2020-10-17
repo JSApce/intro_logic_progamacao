c1df3e843de0ebc6f6cb537b59a0ecb11d68fdb9
jsapce <jamerson.developer@gmail.com>
2020-10-16 22:30:39 -0300

    Verificação da possibilidade de aposentadoria

--- src/CalculaValor.java
+++ src/CalculaValor.java
@@ -4,43 +4,28 @@ public class CalculaValor {
 
 	public static void main(String[] args) {
 		/*
-		 * Faça um programa que receba:
+		 * Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que
+		 * essa pessoa contribuiu com a previdência, depois verifique se ela pode
+		 * aposentar ou não.
 		 * 
-		 * O valor da conta de luz Conta de água Conta de telefone Escola do filho
-		 * Fatura do cartão Gastos com supermercado ... e mostre o gasto total que a
-		 * família teve no mês. Não esqueça de usar muito o operador "+=".
+		 * Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e
+		 * ter contribuído com, pelo menos, 25 anos. Se ela passar nessas duas
+		 * condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
 		 */
 		Scanner scanner = new Scanner(System.in);
-		
-		Double total = 0.0;
-		
-		System.out.println("Cálculo de gastos familiar");
-		System.out.print("Conta de energia: ");
-		Double energia = scanner.nextDouble();
-		total += energia;
-		
-		System.out.print("Conta de água: ");
-		Double agua = scanner.nextDouble();
-		total += agua;
-		
-		System.out.print("Conta de telefone: ");
-		Double telefone = scanner.nextDouble();
-		total += telefone;
-		
-		System.out.print("Conta de escola: ");
-		Double escola = scanner.nextDouble();
-		total += escola;
-		
-		System.out.print("Conta de cartão: ");
-		Double cartao = scanner.nextDouble();
-		total += cartao;
-		
-		System.out.print("Conta de supermercado: ");
-		Double supermercado = scanner.nextDouble();
-		total += supermercado;
-		
-		
-		System.out.println("Total: " + total);
+
+		System.out.println("Verificação da possibilidade de aposentadoria");
+		System.out.print("Sua idade: ");
+		Integer idade = scanner.nextInt();
+
+		System.out.print("Anos de contribuição: ");
+		Integer anoDeContribuicao = scanner.nextInt();
+
+		if (idade >= 55 && anoDeContribuicao >= 25) {
+			System.out.print("Você pode aposentar");
+		} else {
+			System.out.println("Você não pode aposentar");
+		}
 
 		scanner.close();
 	}
