59fc9aa97ab9b9326788837f3634a794f4dbf909
jsapce <jamerson.developer@gmail.com>
2020-10-18 17:30:17 -0300

    Verificar se a pessoa passou no concurso público

--- src/CalculaValor.java
+++ src/CalculaValor.java
@@ -4,27 +4,35 @@ public class CalculaValor {
 
 	public static void main(String[] args) {
 		/*
-		 * Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que
-		 * essa pessoa contribuiu com a previdência, depois verifique se ela pode
-		 * aposentar ou não.
+		 * Crie um programa que vai receber as notas que uma pessoa tirou nas duas
+		 * materias de sua prova. Serão dois parâmetros para receber um para receber a
+		 * nota de português e outro para receber as de matemática.
 		 * 
-		 * Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e
-		 * ter contribuído com, pelo menos, 25 anos. Se ela passar nessas duas
-		 * condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
+		 * A prova, no total, vale 200 pontos - 100 para cada matéria. A nota mínima
+		 * total para passar é igual ou maior que 150. Entretanto, o candidato não pode
+		 * tirar menos do que 60 pontos em qualquer uma das duas matérias, ou seja, se
+		 * tirar 59 em português e 100 em matemática (totalizando uma nota maior do que
+		 * o total necessário que é 150) ele não conseguirá a vaga.
+		 * 
+		 * No final mostre para o candidato se ele conseguiu ou não.
 		 */
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Verificação da possibilidade de aposentadoria");
-		System.out.print("Sua idade: ");
-		Integer idade = scanner.nextInt();
+		System.out.println("Verificar se a pessoa passou no concurso público");
+		System.out.print("Nota de português: ");
+		Integer notaDePortugues = scanner.nextInt();
 
-		System.out.print("Anos de contribuição: ");
-		Integer anoDeContribuicao = scanner.nextInt();
-
-		if (idade >= 55 && anoDeContribuicao >= 25) {
-			System.out.print("Você pode aposentar");
+		System.out.print("Nota de matemática: ");
+		Integer NotaDeMatematica = scanner.nextInt();
+		
+		Boolean notaMaxima = (notaDePortugues + NotaDeMatematica) >= 150;
+		Boolean notaMinimaPortugues = notaDePortugues >= 60;
+		Boolean notaMinimaMatematica = NotaDeMatematica >= 60;
+		
+		if (notaMaxima && notaMinimaPortugues && notaMinimaMatematica) {
+			System.out.print("Aprovado");
 		} else {
-			System.out.println("Você não pode aposentar");
+			System.out.println("Não aprovado");
 		}
 
 		scanner.close();
